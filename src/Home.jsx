411f5bad9e1f7ef97c78d6c5d52bd4ed1fdd8284
athar007 <hussainathar96@gmail.com>
2026-07-23 07:10:24 +0530

    test commit v15

--- src/Home.jsx
+++ src/Home.jsx
@@ -26,7 +26,7 @@ export default function Home() {
 
   const handleDownload = ({currentRole}) => {
     // Native HTML5 download trigger
-    fileName = "";
+    const fileName = "";
     if (currentRole == "fullstack"){
       fileName = "java-fullstack-resume.pdf";
     }
